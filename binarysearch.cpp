6198a288fa588065f70c917646b2c9fa9b18f309
aazamali786 <108629117+aazamali786@users.noreply.github.com>
2022-07-04 00:20:09 +0530

    Create binarysearch.cpp

---
+++ binarysearch.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+bool ispresent(int arr[2][3],int number){
+    for (int i=0;i<2;i++){
+    for(int j=0;j<3;j++){
+        if(arr[i][j]==number){
+            return true;
+        }
+    }
+    return false;
+}
+}
+
+int main(){
+int arr[2][3]={{3,4,7},{33,27,42}};
+for (int i=0;i<2;i++){
+    for(int j=0;j<3;j++){
+        cout<<arr[i][j]<<" ";
+    }
+    cout<<endl;
+}
+int number;
+cin>>number;
+if(ispresent(arr,number)){
+    cout<<"Present";
+}
+else{
+    cout<<"Not Found";
+}
+return 0;
+}
